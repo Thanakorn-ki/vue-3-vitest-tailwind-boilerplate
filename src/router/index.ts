a6c6cff7bb048e9821503b8a9eaf6c9e52788a38
thanakorn-ki <thanakornaum11@gmail.com>
2023-11-17 23:13:36 +0700

    feat: init liff

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LiffPage from '../views/Liff.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'liff',
+      component: LiffPage
     },
     {
       path: '/about',
