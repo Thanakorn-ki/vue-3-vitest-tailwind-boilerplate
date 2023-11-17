8969ebf9d866ea22449299eb29a175df5c70d646
thanakorn-ki <thanakornaum11@gmail.com>
2023-11-17 23:16:40 +0700

    feat: init liff

---
+++ src/views/Liff.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import liff from "@line/liff";
+import { onMounted, ref } from "vue";
+
+const message = ref('')
+const profile = ref({})
+
+onMounted(async () => {
+    try {
+        await liff.init({
+            liffId: import.meta.env.VITE_LIFF_ID
+        })
+        message.value = "LIFF init succeeded.";
+        profile.value = await liff.getProfile()
+
+    } catch (e) {
+        message.value = "LIFF init failed.";
+        console.log(e);
+    }
+})
+</script>
+
+<template>
+    <div class="break-words">
+        <h1>create-liff-app</h1>
+        <div v-if="message">
+            <p>{{ message }}</p>
+            <div>
+                {{ profile }}
+                <img :src="profile.pictureUrl" alt="pictureUrl">
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
